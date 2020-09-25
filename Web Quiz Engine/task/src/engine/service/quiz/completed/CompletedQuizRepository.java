4594353919dd0960450f1e84931e97fb5f1f1ade
MegaOmegaAlpha <bajjramov.vladimir@gmail.com>
2020-09-26 00:18:23 +0400

    fix query mistake in completed quiz select

--- Web Quiz Engine/task/src/engine/service/quiz/completed/CompletedQuizRepository.java
+++ Web Quiz Engine/task/src/engine/service/quiz/completed/CompletedQuizRepository.java
@@ -14,11 +14,11 @@ public interface CompletedQuizRepository extends JpaRepository<CompletedQuizData
 
     @Query(value =
             "select * from quiz_completed qc join quiz q on qc.quiz_id = q.id " +
-            "join user u on q.user_id = u.id where u.email = ?1 " +
-            "order by qc.completedat",
+            "join user u on q.user_id = u.email where u.email = ?1 " +
+            "order by qc.completed_at",
     countQuery =
             "select count(*) from quiz_completed qc join quiz q on qc.quiz_id = q.id " +
-            " join user u on q.user_id = u.id where u.email = ?1",
+            " join user u on q.user_id = u.email where u.email = ?1",
     nativeQuery = true)
     Page<CompletedQuizData> findAllByQuizUser(String userEmail, Pageable pageable);
 
